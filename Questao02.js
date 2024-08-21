6e3e2604c656ce3e0576c31847d48368b880d203
SarahLisley <109000935+SarahLisley@users.noreply.github.com>
2024-08-21 10:06:25 -0300

    Update Questao02.js

--- Questao02.js
+++ Questao02.js
@@ -1,8 +1,8 @@
-import { useState } from "react"
+import { useState } from "react"//Importa o react
 
-import { createContext } from "react";
-import { useContext } from "react"
-export const PokemonContexto = createContext(0)
+import { createContext } from "react";//Cria um contexto
+import { useContext } from "react"//Usa o contexto do react
+export const PokemonContexto = createContext(0)//Cria um contexto
 const Questao02 = () => {
 
     const [emFrente, setFrente] = useState(true);
@@ -11,17 +11,17 @@ const Questao02 = () => {
         setFrente(!emFrente);
     };
 
-    const frenteImagem = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png';
-    const atrasImagem = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png';
+    const frenteImagem = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png';//foto de frente
+    const atrasImagem = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png';//Foto de trás
 
-    const {id} = useContext(PokemonContexto)
+    const {id} = useContext(PokemonContexto)//Usa o contexto para o id
     const url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png/"
 
     return (
         <div>
             <h1>Questao02</h1>
             <img src={emFrente ? frenteImagem : atrasImagem} alt="Pikachu" />
-            <button onClick={toggleImage}>
+            <button onClick={toggleImage}>//Aqui cria o botão para virar para frente e para trás
                 {emFrente ? 'Mostrar Costas' : 'Mostrar Frente'}
             </button>
         </div>
@@ -38,4 +38,4 @@ const Questao02 = () => {
         </div>
     )
 }*/
-export default Questao02
+export default Questao02//exporta a Questao02
