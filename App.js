adb25e66a003fdafa76cc0d0907c0787841a47c6
SarahLisley <109000935+SarahLisley@users.noreply.github.com>
2024-08-21 09:54:40 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import Questao01 from "./components/Prova/Questao01";
+import Questao02 from "./components/Prova/Questao02";
+import Questao03 from "./components/Prova/Questao03";
+import Questao04 from "./components/Prova/Questao04";
+function App() {
+    return (
+        <div>
+        <Questao01 />
+        <Questao02 />
+            <Questao03/>
+            <Questao04/>
+        </div>
+    )
+}
+export default App;
