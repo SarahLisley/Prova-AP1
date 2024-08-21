3befe03021ed8b419d7d59b49980b92101e0d7f6
SarahLisley <109000935+SarahLisley@users.noreply.github.com>
2024-08-21 09:44:55 -0300

    Create Questao01.js

---
+++ Questao01.js
@@ -0,0 +1,38 @@
+import React from "react";
+const Questao01A = () => {//Usei a função arrow para criar o componente
+    const lista = [
+        {a: 10, b: 3, c: 7},
+        {a: 5, b: -3, c: 9},
+        {a: 1, b: 9, c: 40}
+    ]
+
+    return (
+        <div>
+            <Questao01B lista={lista}
+            />
+        </div>
+    )
+}
+
+function Questao01B  (Questao01A) {//Usei function para criar o componente, ou seja, a função clássica
+    const lista = Questao01A
+
+    if (!Array.isArray(lista)) {
+        return <div>Lista não é um array válido</div>;
+    }
+
+    return(
+        <div>
+            <ul>
+            {lista.map((i, index) => {//
+                const ma = Math.max(i.a, i.b, i.c);//Criei um objeto ma para passar os maiores numeros de cada item a, b
+                // e c
+                return <li key={index}> {index + 1}: {ma}</li>;//Aqui eu retorno a posição
+            })}
+            </ul>
+        </div>
+    )
+}
+
+
+export default Questao01A
